fcf1edcc912443e98af93d4625cd2e316223264b
erikaosgue <1476@holbertonschool.com>
2021-05-05 22:25:21 -0400

    Add exercise 9

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise9/FetchAndCacheUsersUseCaseExercise9.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise9/FetchAndCacheUsersUseCaseExercise9.kt
@@ -3,21 +3,35 @@ package com.techyourchance.coroutines.exercises.exercise9
 import com.techyourchance.coroutines.exercises.exercise8.GetUserEndpoint
 import com.techyourchance.coroutines.exercises.exercise8.User
 import com.techyourchance.coroutines.exercises.exercise8.UsersDao
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class FetchAndCacheUsersUseCaseExercise9(
         private val getUserEndpoint: GetUserEndpoint,
         private val usersDao: UsersDao
 ) {
 
-    suspend fun fetchAndCacheUsers(userIds: List<String>) = withContext(Dispatchers.Default) {
-        for (userId in userIds) {
-            val user = getUserEndpoint.getUser(userId)
-            usersDao.upsertUserInfo(user)
+    suspend fun fetchAndCacheUsers(userIds: List<String>):List<User>{
+        return withContext(Dispatchers.Default) {
+
+            val listDeferred = mutableListOf<Deferred<User>>()
+
+            for (userId in userIds){
+                listDeferred.add(
+                    async
+                    {
+                        val user = getUserEndpoint.getUser(userId)
+                        usersDao.upsertUserInfo(user)
+                        user
+                    })
+            }
+	        val listUsers = mutableListOf<User>()
+            for (deferred in listDeferred) {
+                listUsers.add(deferred.await())
+            }
+            listUsers
         }
+
     }
+
 
 }
