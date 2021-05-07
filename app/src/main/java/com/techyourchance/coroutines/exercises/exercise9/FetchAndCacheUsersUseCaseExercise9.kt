15a8c70d89663020793aed709bd572e97ab6b40b
erikaosgue <1476@holbertonschool.com>
2021-05-07 17:40:29 -0400

    Update exercise 9

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise9/FetchAndCacheUsersUseCaseExercise9.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise9/FetchAndCacheUsersUseCaseExercise9.kt
@@ -1,16 +1,19 @@
 package com.techyourchance.coroutines.exercises.exercise9
 
+import android.util.Log
 import com.techyourchance.coroutines.exercises.exercise8.GetUserEndpoint
 import com.techyourchance.coroutines.exercises.exercise8.User
 import com.techyourchance.coroutines.exercises.exercise8.UsersDao
 import kotlinx.coroutines.*
+import kotlin.math.log
+import kotlin.math.pow
 
 class FetchAndCacheUsersUseCaseExercise9(
         private val getUserEndpoint: GetUserEndpoint,
         private val usersDao: UsersDao
 ) {
 
-    suspend fun fetchAndCacheUsers(userIds: List<String>):List<User>{
+    suspend fun fetchAndCacheUsers2(userIds: List<String>):List<User>{
         return withContext(Dispatchers.Default) {
 
             val listDeferred = mutableListOf<Deferred<User>>()
@@ -30,8 +33,21 @@ class FetchAndCacheUsersUseCaseExercise9(
             }
             listUsers
         }
-
     }
 
 
+    suspend fun fetchAndCacheUsers(userIds: List<String>):List<User>{
+        return withContext(Dispatchers.Default) {
+
+            val listUsers = userIds.map { userId ->
+                    async {
+                        Log.d("userId:", userId)
+                        val user = getUserEndpoint.getUser(userId)
+                        usersDao.upsertUserInfo(user)
+                        user
+                    }
+                }.awaitAll()
+            listUsers
+        }
+    }
 }
